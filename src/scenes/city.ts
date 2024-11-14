feb758ea2ce44906a8f3cf35245dc1c63ebc834d
Andres Sweeney-Rios <andressweeneyrios@gmail.com>
2024-11-14 15:15:48 -0800

    add sun

--- src/scenes/city.ts
+++ src/scenes/city.ts
@@ -16,25 +16,28 @@ export const init = async () => {
   const sceneEntId = simulation.EntityRegistry.Create()
   simulation.SimulationState.PhysicsRepository.CreateComponent(sceneEntId)
 
-  const ambientLight = new THREE.AmbientLight(0xff44444, 0.0)
-  scene.add(ambientLight)
+  // const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+  // scene.add(ambientLight)
 
   // add sun
-  // const sun = new THREE.DirectionalLight(0xffffff, 1.0)
-  // sun.position.set(0, 1, 0)
-  // sun.castShadow = true
-  // scene.add(sun)
+  const sun = new THREE.DirectionalLight(0xddccee, 0.2)
+  sun.position.set(0, 1000, 0)
+  sun.target.position.set(0, 0, 0)
+  sun.castShadow = true
+  scene.add(sun)
 
-  // sun.shadow.mapSize.width = 1024; // Higher values provide better shadow quality
-  // sun.shadow.mapSize.height = 1024;
-  // sun.shadow.camera.near = 0.5; // Adjust as needed
-  // sun.shadow.camera.far = 500;  // Adjust as needed
+  sun.shadow.mapSize.width = 1920; // Higher values provide better shadow quality
+  sun.shadow.mapSize.height = 1920;
+  sun.shadow.camera.near = 0.1; // Adjust as needed
+  sun.shadow.camera.far = 5000;  // Adjust as needed
+  sun.shadow.camera.left = -100;
+  sun.shadow.camera.right = 100;
+  sun.shadow.camera.top = 100;
+  sun.shadow.camera.bottom = -100;
+  sun.shadow.bias = -0.001;
 
-  // // For directional lights, configure the shadow camera frustum
-  // sun.shadow.camera.left = -10;
-  // sun.shadow.camera.right = 10;
-  // sun.shadow.camera.top = 10;
-  // sun.shadow.camera.bottom = -10;
+  // scene.fog = new THREE.Fog( 0x000000, 0.1, 10 );
+  // scene.fog = new THREE.FogExp2( 0x000000, 0.1 );
 
   simulation.ViewSync.AddAuxiliaryView(new class ThreeJSRenderer extends View {
     public Draw(): void {
@@ -55,27 +58,26 @@ export const init = async () => {
       scene.background = texture
       scene.backgroundIntensity = 1.0
       scene.environment = texture
-      scene.environmentIntensity = 0.6
-
-      scene.environmentRotation.y = Math.PI / -4
-      scene.backgroundRotation.y = Math.PI / -4
+      scene.environmentIntensity = 0.5
     }),
 
     loadGltf("/3d/scenes/startscene/starterscene.glb")
   ])
 
+  processAttributes(sceneGltf.scene, simulation, sceneEntId, false)
+
   for (const child of traverse(sceneGltf.scene)) {
     if (child instanceof THREE.Mesh) {
       child.castShadow = true
       child.receiveShadow = true
+      const material = child.material as THREE.Material
+      material.shadowSide = THREE.DoubleSide
     }
   }
 
-  processAttributes(sceneGltf.scene, simulation, sceneEntId, false)
-
   scene.add(sceneGltf.scene)
 
-  shaders.applyInjectedMaterials(sceneGltf.scene)
+  // shaders.applyInjectedMaterials(sceneGltf.scene)
 
   simulation.Start()
 
